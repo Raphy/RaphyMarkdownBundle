7b1a7877e4915d5aa172b830ace182259b144f9f
Raphael De Freitas <raphael@de-freitas.net>
2016-07-29 14:31:55 +0200

    Configuring the test suite

---
+++ Tests/DefaultTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Raphy\Symfony\MarkdownBundle\Tests;
+
+/**
+ * Class DefaultTest
+ *
+ * @author Raphael De Freitas <raphael@de-freitas.net>
+ */
+class DefaultTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDefault()
+    {
+        $this->assertTrue(true);
+    }
+}
